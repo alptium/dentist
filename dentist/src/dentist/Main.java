731ecf504726fa5894e85bd511a966791fbd39f9
Vladimir <35075229+vladimirmaljm@users.noreply.github.com>
2018-03-12 22:14:24 +0100

    update

---
+++ dentist/src/dentist/Main.java
@@ -0,0 +1,68 @@
+package dentist;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Dentist app!");
+		
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Do you want to add Doctor? (Y/N)");
+			String a1= sc.next();
+			
+			if ( a1.equals("Y") || a1.equals("y")) {
+				runDemoDoctor();
+			}
+			else if (a1.equals("N") || a1.equals("n")) {
+				System.out.println("You will now enter program for add Patient.");
+				//runDemoPatient();
+			}
+			
+		}
+	}
+
+
+	private static void runDemoDoctor() {
+	
+		System.out.println("Follow the instructions to add new Dentist:");
+	
+		try(Scanner sc = new Scanner(System.in)) {
+				
+			System.out.println("Please enter your firstname: ");
+			String firstName = sc.next();
+			
+			System.out.println("Please enter your lastname: ");
+			String lastName = sc.next();
+			
+			System.out.println("Please enter your birth date: ");
+			int birthDate = sc.nextInt();
+			
+			System.out.println("Please enter your birth mounth: ");
+			int birthMounth = sc.nextInt();
+			
+			System.out.println("Please enter your birth year: ");
+			int birthYear = sc.nextInt();
+			
+			System.out.println("Please enter your JMBG: ");
+			long jmbg = sc.nextLong();
+			
+			if (jmbg < 13 && jmbg > 13) {
+				System.out.println("You entered wrong JMBG number!");
+				System.out.println("Please try again.");
+				long jmbg1 = sc.nextLong();
+				
+				if (jmbg < 13 && jmbg > 13) {
+					System.out.println("You entered wrong JMBG number again!");
+					System.out.println("Please try again, you need to enter 13 numbers.");
+					long jmbg2 = sc.nextLong();
+				}
+			}
+				
+			System.out.println("Please enter your specialization:");
+			String specialization = sc.next();
+				
+			System.out.println("Please enter your licence ID:");
+			int licenceID = sc.nextInt();
+		}
+	}
+}
---
+++ dentist/src/dentist/Patient.java
@@ -0,0 +1,54 @@
+package dentist;
+
+public class Patient {
+	private String name;
+	private String surname;
+	private long jmbg;
+	private int birthDate;
+	private String address;
+	private String city;
+	private int phoneNumber;
+	private String occupation;
+	
+	public Patient(String name, String surname, long jmbg, int birthDate, String address, String city, int phoneNumber, String occupation) {
+		this.name = name;
+		this.surname = surname;
+		this.jmbg = jmbg;
+		this.birthDate = birthDate;
+		this.city = city;
+		this.phoneNumber = phoneNumber;
+		this.occupation = occupation;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public long getJmbg() {
+		return jmbg;
+	}
+	
+	public int getBirthDate() {
+		return birthDate;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public String getOccupation() {
+		return occupation;
+	}
+}
